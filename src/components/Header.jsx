9f6fe7bcceb43411b262d3473a16ee72b4d2fab0
Miriele Silva <mirielea7777@gmail.com>
2025-02-21 20:31:08 -0400

    Dependencias instaladas + instalando templates

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import logo from "../assets/customcolor_icon_transparent_background.png"
+
+const Header = () => {
+  return (<> <img src={logo}width="700px"/><h1>PicView</h1></>)
+     
+}
+export default Header
